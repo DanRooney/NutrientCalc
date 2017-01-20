0c61b63d2709e720ffd8b1db85e5a73613dffddb
Daniel Rooney <danielerooney@gmail.com>
2017-01-19 23:35:09 -0800

    Add files via upload

--- searchBar.js
+++ searchBar.js
@@ -86,3 +86,21 @@ function reset() {
     initialize();
     start();
 }
+
+function removeBars() {
+    for(i=0; i<count; i++){
+        document.getElementById("bp-ac").remove();
+    }
+}
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
